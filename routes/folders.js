9c12e84523dd1bde0fa7f95a44a0821e77c81335
ckarthik <ckarthik@prakya.com>
2025-06-23 01:24:55 +0530

    Added add Folder API

--- routes/folders.js
+++ routes/folders.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const Folder = require('../models/Folder');
 const router = express.Router();
 
 
 //create folder
-router.post('fldr/', async (req, res) => {
-    const {name} = req.body;
-    console.log(name);
+router.post('/user/:userId/fldr/', async (req, res) => {
+    const {title} = req.body;
+    const {userId} = req.params;
     try {
+        console.log('userId', userId,'name', title, req.body );
         // const existing = await User.findOne({email});
         // if(existing) return res.status(400).json({msg: 'User already exsists'});
 
         // const hashed = await bcrypt.hash(password, 10);
-        // const user = new User({username, email, password: hashed});
-        // await user.save();
+        const newFolder = new Folder({name: title, userId:userId});
+        await newFolder.save();
         res.status(201).json({msg: 'Create folder'});
     } catch (error) {
         // console.error('Register error:', error.message); 
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 
 
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/auth', require('./routes/folders'));
 
 const PORT = process.env.PORT || 5000;
 
